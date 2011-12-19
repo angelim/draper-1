cc220b47a28630480b304fc7f58faf1b17c7619c
Alexandre Angelim <angelim@angelim.com.br>
2011-12-19 11:08:28 -0200

    Using #as_json on decorators takes care of generating correct json for DecoratedEnumerableProxy

--- lib/draper/decorated_enumerable_proxy.rb
+++ lib/draper/decorated_enumerable_proxy.rb
@@ -8,14 +8,6 @@ module Draper
       @wrapped_collection, @klass, @options = collection, klass, options
     end
     
-    def to_json(options = {})
-      @wrapped_collection.to_json(options)
-    end
-    
-    def to_xml(options = {})
-      @wrapped_collection.to_xml(options)
-    end
-    
     def each(&block)
       @wrapped_collection.each { |member| block.call(member.decorate(@options)) }
     end
